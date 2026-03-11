3d3fdc27a6bff48e1558deda58680843133e23f2
archana21796 <archana21796@gmail.com>
2026-03-11 10:46:51 +0530

    added new admissions

--- admissions/contact-enquiry-for-admission.php
+++ admissions/contact-enquiry-for-admission.php
@@ -107,6 +107,34 @@ if (!$stmt->execute()) {
 
 $stmt->close();
 
+/* Whatsapp bot */
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://webhooksapp.vleafy.com/bot/inbound/69a83dd602e28c7ee4e1dd5a',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+    "name": "'.$sender_name.'",
+    "number": "91'.$sender_mobile.'"
+}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+/* Mail Setup */
+
 $mail->isSMTP();            
 //Set SMTP host name                          
 $mail->Host = "mail.onemg.com";
